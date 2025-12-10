badebecfb6e2ca375bdbc4fa7279924f41acaf05
joysondsouza47 <joysondsouza47@gmail.com>
2025-12-10 08:10:13 +0530

    Update Lab001_HelloWorld.java

--- src/ex_01_Java_Basics/Lab001_HelloWorld.java
+++ src/ex_01_Java_Basics/Lab001_HelloWorld.java
@@ -3,5 +3,6 @@ package ex_01_Java_Basics;
 public class Lab001_HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("welcome to java programs");
     }
 }
