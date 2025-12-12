d5df128803de75839dd2948f6262af7923c0a41d
joysondsouza47 <joysondsouza47@gmail.com>
2025-12-12 08:35:51 +0530

    commit

--- src/ex_01_Java_Basics/Lab001_HelloWorld.java
+++ src/ex_01_Java_Basics/Lab001_HelloWorld.java
@@ -4,5 +4,6 @@ public class Lab001_HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("welcome to java programs");
+        System.out.println("welcome");
     }
 }
---
+++ src/ex_01_Java_Basics/Lab002_welcome.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basics;
+
+public class Lab002_welcome {
+    static void main() {
+        System.out.println("welcome1");
+        System.out.println("welcome2");
+        System.out.println("welcome3");
+        System.out.println("welcome4");
+        System.out.println("welcome5");
+
+
+    }
+}
