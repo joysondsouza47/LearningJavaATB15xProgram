bd0b03fbf9a44fa38dea7cb8b397b5f51e6bb3bd
joysondsouza47 <joysondsouza47@gmail.com>
2025-12-12 08:37:21 +0530

    Update Lab002_welcome.java

--- src/ex_01_Java_Basics/Lab002_welcome.java
+++ src/ex_01_Java_Basics/Lab002_welcome.java
@@ -2,12 +2,6 @@ package ex_01_Java_Basics;
 
 public class Lab002_welcome {
     static void main() {
-        System.out.println("welcome1");
-        System.out.println("welcome2");
-        System.out.println("welcome3");
-        System.out.println("welcome4");
-        System.out.println("welcome5");
-
-
+        System.out.println("welcome joyson");
     }
 }
