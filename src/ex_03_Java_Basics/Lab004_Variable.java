b66befc08dc225b0a40b98524fe6d1bbfc287616
joysondsouza47 <joysondsouza47@gmail.com>
2025-12-17 09:21:01 +0530

    commit 17-12-25

---
+++ src/ex_03_Java_Basics/Lab004_Variable.java
@@ -0,0 +1,38 @@
+package ex_03_Java_Basics;
+
+public class Lab004_Variable {
+    public static void main(String[] args){
+        // data type variable name = variable name
+        byte value_1 = 45;
+        value_1 = 127;
+        // now new value for the variable value_1 is 127
+        // I want to capture age of a person, which data type to be used - we can use byte data type
+        // byte can store from -128 to 127
+        // can we create a variable with a underscore and dollar (only these are allowed A-Z, a-z, _, $,)
+        // int _123 = 123
+        // System.out.println(_123);
+        // yes this one will be executed.
+        // int enum = 10
+        // is this a valid syntax or not - here enum is a reserved keyword so we cannot use a enum key word as identifier.
+        short s = 123;
+        int i = 234;
+        long l = 9945636877L;  // to store phone number
+        // long has a special property if we want to use the long datatype, we have to use l in the end... 9945636877l / 9945636877L
+        char c = 'A';
+        char r = 90;
+        // Char is actually an integral so we are not getting an error.
+        float f = 23.567F;
+        // float has a special property if we want to use the float datatype, we have to use f in the end... 9945.45435f / 9945636877F
+        System.out.println(value_1);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(c);
+        System.out.println(r);
+        System.out.println(f);
+
+        String string = "joyson";
+        // non-primitive
+        // string is a bunch of chars.
+    }
+}
---
+++ src/ex_03_Java_Basics/Lab_005_Printf.java
@@ -0,0 +1,21 @@
+package ex_03_Java_Basics;
+
+public class Lab_005_Printf {
+    public static void main() {
+        int num = 9;
+
+        System.out.println("Multiplication of 9");
+
+        System.out.printf("%d x  1 = %2d%n",num,num);
+        System.out.printf("%d x  2 = %d%n",num,num*2);
+        System.out.printf("%d x  3 = %d%n",num,num*3);
+        System.out.printf("%d x  4 = %d%n",num,num*4);
+        System.out.printf("%d x  5 = %d%n",num,num*5);
+        System.out.printf("%d x  6 = %d%n",num,num*6);
+        System.out.printf("%d x  7 = %d%n",num,num*7);
+        System.out.printf("%d x  8 = %d%n",num,num*8);
+        System.out.printf("%d x  9 = %d%n",num,num*8);
+        System.out.printf("%d x 10 = %d",num,num*10);
+
+    }
+}
