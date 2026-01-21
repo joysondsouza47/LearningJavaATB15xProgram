0d29cb485886043e974da63a4d8314c2e128a94a
joysondsouza47 <joysondsouza47@gmail.com>
2026-01-21 07:56:26 +0530

    21/01/2026

---
+++ src/ex_04_Java_basic_Program/P43_start_pyramid.java
@@ -0,0 +1,22 @@
+package ex_04_Java_basic_Program;
+
+import java.util.Scanner;
+
+public class P43_start_pyramid {
+    public static void main(String [] args)
+    {
+        Scanner sc=new Scanner(System.in) ;
+        System.out.println("enter the numner n");
+        int n=sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
