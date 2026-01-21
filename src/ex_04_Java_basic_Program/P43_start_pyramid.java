8dbed53c0d1542a1480235ca92d64bfeb2acd4f4
joysondsouza47 <joysondsouza47@gmail.com>
2026-01-21 09:39:55 +0530

    21_01_26_1

--- src/ex_04_Java_basic_Program/P43_start_pyramid.java
+++ src/ex_04_Java_basic_Program/P43_start_pyramid.java
@@ -6,7 +6,7 @@ public class P43_start_pyramid {
     public static void main(String [] args)
     {
         Scanner sc=new Scanner(System.in) ;
-        System.out.println("enter the numner n");
+        System.out.println("enter the number n");
         int n=sc.nextInt();
 
         for (int i = 1; i <= n; i++) {
