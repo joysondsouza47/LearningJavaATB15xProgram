129b3a3174f1b4fff5344387acb3573a9b412d15
joysondsouza47 <joysondsouza47@gmail.com>
2026-01-30 06:54:03 +0530

    30/01/2026_1

--- src/ex_11_OOPS_Concepts/ex_08_Encapsulation/P01_private_keyword.java
+++ src/ex_11_OOPS_Concepts/ex_08_Encapsulation/P01_private_keyword.java
@@ -1,4 +1,24 @@
 package ex_11_OOPS_Concepts.ex_08_Encapsulation;
 
 public class P01_private_keyword {
+    private int accnum;
+    private String name;
+    private double amount;
+
+    public int getAcconum() {
+        System.out.println("your account number is:");
+        return accnum;
+    }
+
+    public void setAcconum(int accnum) {
+        this.accnum = accnum;
+    }
+
+
+    public static void main(String[] args) {
+        P01_private_keyword pr = new P01_private_keyword();
+        pr.setAcconum(10506);
+        System.out.println(pr.getAcconum());
+
+    }
 }
