56e16d13ab03537d27905498034d969e4af22875
joysondsouza47 <joysondsouza47@gmail.com>
2026-01-31 18:15:30 +0530

    31/01/2026_2

--- src/ex_11_OOPS_Concepts/ex_09_Abstraction/Main.java
+++ src/ex_11_OOPS_Concepts/ex_09_Abstraction/Main.java
@@ -3,10 +3,16 @@ package ex_11_OOPS_Concepts.ex_09_Abstraction;
 public class Main {
     public static void main(String[] args)
     {
-        demo obj1 = new demo();
+        demo obj1 = new demo();  // class object with class reference
         obj1.circle();       //abstract method
         obj1.square();       //default method
         shape.rectangle();   //static method
         obj1.triangle();     //normal method
+
+        shape obj2 = new demo();  // interface reference with class object
+        obj2.circle();            // abstract method
+        obj2.square();            // default method
+        shape.rectangle();        // static method
     }
+
 }
