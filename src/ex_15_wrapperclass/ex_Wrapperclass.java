f702597f01c85c4421b8ced52a7c9590ca16108d
joysondsouza47 <joysondsouza47@gmail.com>
2026-02-07 11:43:16 +0530

    07/02/2026_1

--- src/ex_15_wrapperclass/ex_Wrapperclass.java
+++ src/ex_15_wrapperclass/ex_Wrapperclass.java
@@ -1,5 +1,12 @@
 package ex_15_wrapperclass;
 
 public class ex_Wrapperclass {
+    public static void main(String[] args)
+    {
+        int a = 10;
+        int b = 20;
+        System.out.println("my name is joyson");
+        System.out.println(a+b);
+    }
 
 }
