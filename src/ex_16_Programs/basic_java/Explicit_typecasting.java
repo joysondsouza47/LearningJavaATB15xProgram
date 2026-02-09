a8a79c93ef724da37239f3f3018dc08dec2fefb0
joysondsouza47 <joysondsouza47@gmail.com>
2026-02-09 07:26:43 +0530

    09/02/2026_2

--- src/ex_16_Programs/basic_java/Explicit_typecasting.java
+++ src/ex_16_Programs/basic_java/Explicit_typecasting.java
@@ -4,6 +4,12 @@ public class Explicit_typecasting {
 
     public static void main(String[] args)
     {
+          double a = 34.67;
+          int b = (int) a;
+
+        System.out.println(b);
+        System.out.println(a);
+
 
     }
 }
